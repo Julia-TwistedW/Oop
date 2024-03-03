fcc80231cac7963e407c1878178bb6b48e8ae4ed
GrigoryGrigory <badguy0bs@gmail.com>
2024-03-03 13:18:15 +0300

    zoo

--- src/main/kotlin/inheritance.kt
+++ src/main/kotlin/inheritance.kt
@@ -1,27 +1,25 @@
-open class Person(protected val name: String, val age: Int) {
+open class Animal(protected val name: String, ) {
     fun walk(){
         println("$name can walk")
     }
 }
-class Driver(name: String, age: Int, val experience: Int): Person(name, age) {
-    fun drive(){
-        println("$name can walk")
+class Cats (name: String, val type: String, val from: String): Animal(name) {
+    fun cats(){
+        println("$name can hunt")
     }
 }
-class Teacher(name: String, age: Int,val subject: String): Person(name, age)
+class Bird(name: String, val type: String, val from: String): Animal(name) {
+    fun birds(){
+        println("$name can fly")
+    }
+}
 
 fun main(){
-    val person1 = Person("Ian", 16)
-//    person1.walk()
-    val person2 = Person("Avas", 15)
-//    person2.walk()
-    val driver1 = Driver("Carl", 33,10)
-//    driver1.walk()
-//    driver1.drive()
-    val teacher1 = Teacher ("Ludmila Petrova", 40, "chemist")
-//    teacher1.walk()
-    val people = listOf<Person>(person1, person2, driver1, teacher1)
-    for (person in people) {
-        person.walk()
+    val animal = Animal("cat")
+    val jaguar = Cats("Carl", "predator", "Africa")
+    val hawk = Bird("Emma", "predator", "Russia")
+    val animals = listOf<Animal>(animal, jaguar, hawk)
+    for (zoo in animals) {
+        zoo.walk()
     }
 }
